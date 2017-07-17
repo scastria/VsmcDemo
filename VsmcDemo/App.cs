3ee545221b2291085019b5a2dfe9a7c45e3467bf
shawn <shawn@spcware.com>
2017-07-16 22:14:26 -0500

    Initial checkin

---
+++ VsmcDemo/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Xamarin.Forms;
+
+namespace VsmcDemo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new FirstPage();
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            MobileCenter.Start("ios=cc85ffa0-9495-453d-b6de-789f5e3eb9ab;" +
+				   "uwp={Your UWP App secret here};" +
+				   "android={Your Android App secret here}",
+				   typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ VsmcDemo/FirstPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace VsmcDemo
+{
+    public class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            BackgroundColor = Color.Red;
+            Title = "VsmcDemo";
+            Button btn = new Button {
+                Text = "Crash Me!"
+            };
+            btn.Clicked += (sender, e) => {
+                string crash = null;
+                Console.WriteLine(crash.Length);
+            };
+            Content = new StackLayout {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    btn
+                }
+            };
+    	}
+    }
+}
